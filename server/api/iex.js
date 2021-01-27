bb3422788cc65ace9a6ad6ac264de6a47e52aabc
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2021-01-26 19:57:30 -0500

    committed files

--- server/api/iex.js
+++ server/api/iex.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api.iextrading.com/1.0'
+  baseURL: 'https://cloud.iexapis.com/stable'
 })
 
 export function loadQuoteForStock(symbol) {
-  return api.get(`/stock/${symbol}/quote`).then(res => {
+  return api.get(`/stock/${symbol}/quote?token=pk_949cbfb4b4ac4abb82181f7ac5a93080`).then(res => {
     return res.data
   })
 }
 
 export function loadStockSymbols() {
-  return api.get(`/ref-data/symbols`).then(res => {
+  return api.get(`/ref-data/symbols?token=pk_949cbfb4b4ac4abb82181f7ac5a93080`).then(res => {
     return res.data
   })
 }
@@ -20,7 +20,7 @@ export function loadBatchStocks(stockSymbols) {
   if (stockSymbols !== null && stockSymbols.length !== 0) {
     return api
       .get(
-        `/stock/market/batch?symbols=${stockSymbols}&types=quote,chart&range=2y&last=4`
+        `/stock/market/batch?token=pk_949cbfb4b4ac4abb82181f7ac5a93080&symbols=${stockSymbols}&types=quote,chart&range=2y`
       )
       .then(res => {
         return res.data
